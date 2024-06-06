adb311bb3ad7dcec333687e4a4a7bd87d2722b37
lulusbagos <lulusbagoshermawan@gmail.com>
2024-06-06 11:11:29 +0800

    update

---
+++ BringHome/BringHome/Controllers/SparingController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BringHome.Controllers
+{
+    public class SparingController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
